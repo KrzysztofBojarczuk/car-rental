905a5866fb7a5a9464d535d0928c12de19398cba
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-06-22 16:25:49 +0200

    Member

--- RentalCarWebApi/RentalCarWebApi/InterafceRepository/IMemberRepository.cs
+++ RentalCarWebApi/RentalCarWebApi/InterafceRepository/IMemberRepository.cs
@@ -5,5 +5,9 @@ namespace RentalCarWebApi.InterafceRepository
     public interface IMemberRepository
     {
         Task<List<Member>> GetAllMemebersAsync();
+        Task<Member> GetMemeberByIdAsync(int id);
+        Task<Member> CreateMemberAsync(Member member);
+        Task<Member> UpdateMemberAsync(Member updateMember);
+        Task<Member> DeleteMemberAsync(int id);
     }
 }
